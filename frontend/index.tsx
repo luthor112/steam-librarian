9097fe4c0bb2aab94839b4776cac798d15e17a59
luthor112 <luthor112@gmail.com>
2025-08-04 16:34:41 +0200

    Integrate new load fix, update libs

--- frontend/index.tsx
+++ frontend/index.tsx
@@ -142,12 +142,7 @@ async function OnPopupCreation(popup: any) {
 
 export default async function PluginMain() {
     console.log("[steam-librarian] frontend startup");
-    while (
-        typeof g_PopupManager === 'undefined' ||
-        typeof MainWindowBrowserManager === 'undefined'
-    ) {
-        await sleep(100);
-    }
+    await App.WaitForServicesInitialized();
 
     // Call the backend methods and log the configuration
     const gameName = await get_autoselect_item({});
